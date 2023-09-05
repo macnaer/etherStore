93e5286331d3c3392a696487c2482885026dc9fa
macnaer <trofimchuk.an@gmail.com>
2023-09-05 18:17:46 +0300

    Update

---
+++ components/providers/web3/hooks/useAccount.js
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import useSWR from "swr";
+
+const adminAddresses = {
+  "0x3947781ca299acd894c1a11dc698e3a5de84dac806efc6cf22cda7b7ac243db3": true,
+};
+
+export const handler = (web3, provider) => () => {
+  const { data, mutate, ...rest } = useSWR(
+    () => (web3 ? "web3/accounts" : null),
+    async () => {
+      const accounts = await web3.eth.getAccounts();
+      const account = accounts[0];
+
+      if (!account) {
+        throw new Error(
+          "Cannot retreive an account. Please refresh the browser."
+        );
+      }
+
+      return account;
+    }
+  );
+
+  useEffect(() => {
+    const mutator = (accounts) => mutate(accounts[0] ?? null);
+    provider?.on("accountsChanged", mutator);
+
+    return () => {
+      provider?.removeListener("accountsChanged", mutator);
+    };
+  }, [provider]);
+
+  return {
+    data,
+    isAdmin: (data && adminAddresses[web3.utils.keccak256(data)]) ?? false,
+    mutate,
+    ...rest,
+  };
+};
